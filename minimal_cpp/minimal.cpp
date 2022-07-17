62ef81e8bc5003a838eafd0837a899fd15ee03a7
pinarica <46538132+pinarica@users.noreply.github.com>
2022-07-16 21:20:40 -0400

    Add files via upload

---
+++ minimal_cpp/minimal.cpp
@@ -0,0 +1,9 @@
+#include <iostream>
+
+#include "qpp.h"
+
+int main() {
+    using namespace qpp;
+    std::cout << "Hello Quantum++!\nThis is the |0> state:\n";
+    std::cout << disp(0_ket) << '\n';
+}
---
+++ serial/run.sh
@@ -0,0 +1,3 @@
+
+g++ -std=c++11 -isystem $HOME/eigen -I $HOME/qpp/include grover.cpp -o grover
+./grover
